f74db60903837728de268528e7e016d9b1b5038b
Spicciani <marco.spicciani@gmail.com>
2022-05-23 06:42:31 +0200

    Aggiunto colore

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
+            Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
             Text("Hello, world!")
                 .padding()
             Text("Ciao Pippo")
