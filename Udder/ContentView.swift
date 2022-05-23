2ef42d6264ac1be7a1e5c17e378ef848c842f58f
Spicciani <marco.spicciani@gmail.com>
2022-05-23 06:32:19 +0200

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by MacMini on 23/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("Hello, world!")
+                .padding()
+            Text("Ciao Pippo")
+                .padding()
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by MacMini on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
